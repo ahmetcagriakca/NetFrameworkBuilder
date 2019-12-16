f43bcb842bb7ddf90b52320b08bf70a36db67d9a
Ahmet Çağrı AKCA <ahmetcakca@hotmail.com>
2019-12-16 15:53:47 +0300

    Builder output modified

--- Build.py
+++ Build.py
@@ -1,12 +1,12 @@
-import MsBuilder, SolutionFinder
+import MsBuilder
+import SolutionFinder
 rootFolder="ProjectFolder"
-if result == False:
-    print('build failed')
-
+excludedProjects = ["ExcludeSomePath"]
 solutionFinder = SolutionFinder.SolutionFinder(rootFolder)
 msBuilder = MsBuilder.MsBuilder()  # create our instance
 for solutionPath in solutionFinder.Solutions:
-    result = msBuilder.run(solutionPath)
-    if result == False:
-        print('build failed')
-
+    if solutionPath not in excludedProjects:
+        print(solutionPath)
+        result = msBuilder.run(solutionPath)
+        if result == False:
+            print('build failed')
